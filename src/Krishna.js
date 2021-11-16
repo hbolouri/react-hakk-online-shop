3df5f2211cd80a370e0045f865a9ae0fffc02c8b
KrishnaA01 <mahato77@hotmail.com>
2021-11-16 09:23:42 +0100

    my name

---
+++ src/Krishna.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Krishna() {
+    return (
+        <div>
+           <h1>this is Krishna</h1>
+           <h1>this is mahato</h1>
+        </div>
+    )
+}
