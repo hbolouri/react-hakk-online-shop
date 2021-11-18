418bab95f7b35b2ba4c7b50ded4d004463a26f8e
Kanyshai Sharapova <kanysharapova@gmail.com>
2021-11-18 13:28:26 +0100

    remove node

---
+++ src/components/Payment/Payment.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import './Payment.css'
+
+
+export default function Payment() {
+    return (
+        <div className='Payment'>
+            <h1>Payment</h1>
+
+            <div className='Personal-info'>
+
+            </div>
+
+            <div className='Total-price'>
+
+            </div>
+        </div>
+    )
+}
