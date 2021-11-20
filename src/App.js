7015200202a23bc5da6f180a695d4c85c44f8393
ademten82 <ademten7@gmail.com>
2021-11-20 11:20:39 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import Contact from "./components/Contact/Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
-import Men from './pages/Men/Men'
-import './App.css'
+import Men from "./pages/Men/Men";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Men />
-      <Contact />
     </div>
   );
 }
