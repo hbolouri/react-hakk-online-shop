faf9a4cf3e5209f4e8d66260a01a896792105aa0
KrishnaA01 <mahato77@hotmail.com>
2021-11-22 13:43:16 +0100

    finished men, women, kid and add to shopping bag

--- src/pages/Men/Men.js
+++ src/pages/Men/Men.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import './Men.css'
 import { menClothes, womenClothes, kidsClothes } from '../../api/products'
-import allClothes from '../../api/products'
 import Products from '../../components/Section/Products'
 import Section from '../../components/Section/Section'
 
@@ -14,20 +13,24 @@ export default function Men() {
     return (
         <main >
             <Section title="Men's Collection" data={ menClothes } />
-                <div className="images-parent">
-                    <div className="image-left">
-
-                    <div className="image-text">
-                        <h1 style={{color:"white"}}>A Chill Season ~</h1>
+            
+                <div className="images-parent-m">
+                    <div className="image-left-m">
+                    <div className="image-text-m">
+                        <h1 style={{color:"white"}}>A Chill Season ~~~</h1>
                         <h5 style={{color:"white"}}>Effortless design to wear anywhere</h5>
                         <button className="btn-image">SHOP NOW</button>
                     </div> 
                     </div>
-                    <div className="image-right">
+                    <div className="image-right-m">
                     </div>
                 </div>
-            <Products title="All Products" data={allClothes} />   
-                        
+
+            <div className="men-all">
+                <Products  title="All Products" data={menClothes} />
+                
+            </div>         
         </main>
     )
 
+    }
--- src/pages/Women/Women.js
+++ src/pages/Women/Women.js
@@ -1,7 +1,36 @@
-import React from "react";
 
-const Women = () => {
-  return <div>Women</div>;
-};
+import React from 'react'
+import './Women.css'
+import { menClothes, womenClothes, kidsClothes } from '../../api/products'
+import Products from '../../components/Section/Products'
+import Section from '../../components/Section/Section'
 
-export default Women;
+
+
+
+export default function Women() {
+
+    return (
+        <main >
+            <Section title="Women's Collection" data={ womenClothes } />
+            
+                <div className="images-parent-w">
+                    <div className="image-left-w">
+                    <div className="image-text-w">
+                        <h1 style={{color:"white"}}>A Chill Season ~~~</h1>
+                        <h5 style={{color:"white"}}>Effortless design to wear anywhere</h5>
+                        <button className="btn-image">SHOP NOW</button>
+                    </div> 
+                    </div>
+                    <div className="image-right-w">
+                    </div>
+                </div>
+
+            <div className="women-all">
+                <Products  title="All Products" data={ womenClothes } />
+                
+            </div>         
+        </main>
+    )
+
+    }
