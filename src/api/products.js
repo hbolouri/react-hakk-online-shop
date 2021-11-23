0e6c7aaeb2de34832fcfd1373fd7ae8b3bdfda5a
KrishnaA01 <mahato77@hotmail.com>
2021-11-23 13:00:22 +0100

    added styles on button

--- src/api/products.js
+++ src/api/products.js
@@ -14,7 +14,7 @@ export const menClothes = [
     },
     {
         id: 2,
-        title: "Mens Casual Premium Slim Fit Shirts ",
+        title: "Mens Casual Slim Fit Shirts ",
         price: 22.3,
         description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
         category: "men's clothing",
@@ -50,11 +50,11 @@ export const menClothes = [
     },
     {
         id: 86,
-        title: "Mens Casual Slim Fit",
+        title: "Harley Davidson t Shirt",
         price: 15.99,
         description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
         category: "men's clothing",
-        image: "https://sc02.alicdn.com/kf/H3554a8539bd947d5a9d682275d7c667bb/formal-shirts-and-pants-combination-men-formal.png",
+        image: "https://m.media-amazon.com/images/I/610KY73m3wL._AC_UX342_.jpg",
         rating: {
         rate: 2.1,
         count: 430
@@ -74,11 +74,11 @@ export const menClothes = [
     },
     {
         id: 88,
-        title: "Mens Casual Slim Fit",
+        title: "Mens Bomber Jacket",
         price: 15.99,
         description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
         category: "men's clothing",
-        image: "https://sc02.alicdn.com/kf/H3554a8539bd947d5a9d682275d7c667bb/formal-shirts-and-pants-combination-men-formal.png",
+        image: "https://i0.wp.com/ae01.alicdn.com/kf/HTB1L07xiaQoBKNjSZJnq6yw9VXaI/-font-b-Men-b-font-jeans-font-b-jacket-b-font-brand-new-fashion-font.jpg",
         rating: {
         rate: 2.1,
         count: 430
@@ -86,11 +86,11 @@ export const menClothes = [
     },
     {
         id: 89,
-        title: "Mens Casual Slim Fit",
+        title: "Mens Nike Down Jacket",
         price: 15.99,
         description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
         category: "men's clothing",
-        image: "https://sc02.alicdn.com/kf/H3554a8539bd947d5a9d682275d7c667bb/formal-shirts-and-pants-combination-men-formal.png",
+        image: "https://i.pinimg.com/originals/60/7e/4b/607e4b842a727c3492b4f13498cc5fef.jpg",
         rating: {
         rate: 2.1,
         count: 430
@@ -115,7 +115,7 @@ export const womenClothes = [
     },
     {
             id: 6,
-            title: "Removable Hooded Faux Leather",
+            title: "Removable Hooded Leather",
             price: 29.95,
             description: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
             category: "women's clothing",
@@ -139,7 +139,7 @@ export const womenClothes = [
     },
     {
             id: 8,
-            title: "Short Sleeve Boat Neck V ",
+            title: "Short Sleeve Boat V ",
             price: 9.85,
             description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
             category: "women's clothing",
@@ -174,11 +174,11 @@ export const womenClothes = [
             }
     },{
         id: 1125,
-        title: "DANVOUY Cotton Short",
+        title: "Cotton V-neck T Shirt",
         price: 12.99,
         description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
         category: "women's clothing",
-        image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+        image: "https://i5.walmartimages.com/asr/a5e15b0c-c4e9-4943-a495-9ecbec37bd9f_1.5455ecd0d09089578a2c18e709910058.jpeg",
         rating: {
         rate: 3.6,
         count: 145
@@ -186,11 +186,11 @@ export const womenClothes = [
 },
 {
     id: 112,
-    title: "DANVOUY Womens T Shirt",
+    title: "Long Sleeve Jumper",
     price: 12.99,
     description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
     category: "women's clothing",
-    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    image: "https://www.fruitoftheloom.eu/wp-content/uploads/61-404-hd_dark-heather-grey_front.jpg",
     rating: {
     rate: 3.6,
     count: 145
@@ -203,7 +203,7 @@ export const kidsClothes = [
     { 
         id: 11,
         title: "premium cotton baby's wear",
-        price: 695,
+        price: 55,
         description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
         category: "jewelery",
         image: "https://4.imimg.com/data4/IA/WC/MY-819866/ladies-printed-t-shirt-500x500.jpg",
@@ -214,7 +214,7 @@ export const kidsClothes = [
     },
     {
         id: 12,
-        title: "pyjamas for your little kido ",
+        title: "pyjamas for your little kido",
         price: 168,
         description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
         category: "jewelery",
--- src/components/Section/Card/Card.js
+++ src/components/Section/Card/Card.js
@@ -3,7 +3,7 @@ import "./Card.css";
 import { Link } from "react-router-dom";
 
 export default function Card({ product }) {
-  console.log(product);
+  
   return (
     <div className="Card" style={{ backgroundImage: `url(${product.image})` }}>
       <Link className="remove-underline" to={`/singleProduct/${product.id}`}>{product.title}</Link>
