6f2e98e02ddc3f9c4536a16bb23c347e5f57461a
ademten82 <ademten7@gmail.com>
2021-11-24 08:43:51 +0100

    updated ids of  product.js

--- src/api/products.js
+++ src/api/products.js
@@ -25,21 +25,23 @@ export const menClothes = [
     rating: {
       rate: 4,
       count: 259,
-    }
-  },
-    {
-        id: 2,
-        title: "Mens Casual Slim Fit Shirts ",
-        price: 22.3,
-        description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        category: "men's clothing",
-        image: "https://sc01.alicdn.com/kf/HTB1eD8.PrvpK1RjSZPiq6zmwXXaT/New-formal-shirts-and-pants-combination-luxury.jpg",
-        rating: {
-        rate: 4.1,
-        count: 259
-        }
     },
- 
+  },
+  {
+    id: 3,
+    title: "Mens Casual Slim Fit Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://sc01.alicdn.com/kf/HTB1eD8.PrvpK1RjSZPiq6zmwXXaT/New-formal-shirts-and-pants-combination-luxury.jpg",
+    rating: {
+      rate: 4.1,
+      count: 259,
+    },
+  },
+
   {
     id: 4,
     title: "Mens Casual Slim Fit",
@@ -51,7 +53,7 @@ export const menClothes = [
     rating: {
       rate: 2,
       count: 430,
-    }
+    },
   },
   {
     id: 86,
@@ -65,22 +67,22 @@ export const menClothes = [
     rating: {
       rate: 2,
       count: 430,
-    }
-  },
-    {
-      
-        id: 86,
-        title: "Harley Davidson t Shirt",
-        price: 15.99,
-        description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-        category: "men's clothing",
-        image: "https://m.media-amazon.com/images/I/610KY73m3wL._AC_UX342_.jpg",
-        rating: {
-        rate: 2.1,
-        count: 430
-        }
     },
-  
+  },
+  {
+    id: 87,
+    title: "Harley Davidson t Shirt",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image: "https://m.media-amazon.com/images/I/610KY73m3wL._AC_UX342_.jpg",
+    rating: {
+      rate: 2.1,
+      count: 430,
+    },
+  },
+
   {
     id: 88,
     title: "Mens Casual Slim Fit",
@@ -93,34 +95,37 @@ export const menClothes = [
     rating: {
       rate: 2,
       count: 430,
-    }
-  },
-    {
-        id: 88,
-        title: "Mens Bomber Jacket",
-        price: 15.99,
-        description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-        category: "men's clothing",
-        image: "https://i0.wp.com/ae01.alicdn.com/kf/HTB1L07xiaQoBKNjSZJnq6yw9VXaI/-font-b-Men-b-font-jeans-font-b-jacket-b-font-brand-new-fashion-font.jpg",
-        rating: {
-        rate: 2.1,
-        count: 430
-        }
     },
-    {
-        id: 89,
-        title: "Mens Nike Down Jacket",
-        price: 15.99,
-        description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-        category: "men's clothing",
-        image: "https://i.pinimg.com/originals/60/7e/4b/607e4b842a727c3492b4f13498cc5fef.jpg",
-        rating: {
-        rate: 2.1,
-        count: 430
-        }
-    }
-
-]
+  },
+  {
+    id: 89,
+    title: "Mens Bomber Jacket",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image:
+      "https://i0.wp.com/ae01.alicdn.com/kf/HTB1L07xiaQoBKNjSZJnq6yw9VXaI/-font-b-Men-b-font-jeans-font-b-jacket-b-font-brand-new-fashion-font.jpg",
+    rating: {
+      rate: 2.1,
+      count: 430,
+    },
+  },
+  {
+    id: 90,
+    title: "Mens Nike Down Jacket",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image:
+      "https://i.pinimg.com/originals/60/7e/4b/607e4b842a727c3492b4f13498cc5fef.jpg",
+    rating: {
+      rate: 2.1,
+      count: 430,
+    },
+  },
+];
 
 export const womenClothes = [
   {
@@ -134,20 +139,20 @@ export const womenClothes = [
     rating: {
       rate: 2.6,
       count: 235,
-    }
+    },
   },
-    {
-            id: 6,
-            title: "Removable Hooded Leather",
-            price: 29.95,
-            description: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
-            category: "women's clothing",
-            image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-            rating: {
-            rate: 2.9,
-            count: 340
-            }
-    
+  {
+    id: 6,
+    title: "Removable Hooded Leather",
+    price: 29.95,
+    description:
+      "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    rating: {
+      rate: 2.9,
+      count: 340,
+    },
   },
   {
     id: 7,
@@ -160,21 +165,22 @@ export const womenClothes = [
     rating: {
       rate: 2,
       count: 679,
-    }
-  },
-    {
-            id: 8,
-            title: "Short Sleeve Boat V ",
-            price: 9.85,
-            description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
-            category: "women's clothing",
-            image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
-            rating: {
-            rate: 4.7,
-            count: 130
-            }
     },
-  
+  },
+  {
+    id: 8,
+    title: "Short Sleeve Boat V ",
+    price: 9.85,
+    description:
+      "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    rating: {
+      rate: 4.7,
+      count: 130,
+    },
+  },
+
   {
     id: 9,
     title: "Sleeve Moisture",
@@ -186,40 +192,44 @@ export const womenClothes = [
     rating: {
       rate: 2,
       count: 146,
-    }
-  },
-    {
-            id: 1136,
-            title: "DANVOUY Casual Cotton Short",
-            price: 12.99,
-            description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-            category: "women's clothing",
-            image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
-            rating: {
-            rate: 3.6,
-            count: 145
-            }
     },
-    {
-        id: 1125,
-        title: "Cotton V-neck T Shirt",
-        price: 12.99,
-        description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-        category: "women's clothing",
-        image: "https://i5.walmartimages.com/asr/a5e15b0c-c4e9-4943-a495-9ecbec37bd9f_1.5455ecd0d09089578a2c18e709910058.jpeg",
-        rating: {
-        rate: 3.6,
-        count: 145
-        }
-},
-{
+  },
+  {
+    id: 1136,
+    title: "DANVOUY Casual Cotton Short",
+    price: 12.99,
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    rating: {
+      rate: 3.6,
+      count: 145,
+    },
+  },
+  {
+    id: 1125,
+    title: "Cotton V-neck T Shirt",
+    price: 12.99,
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    category: "women's clothing",
+    image:
+      "https://i5.walmartimages.com/asr/a5e15b0c-c4e9-4943-a495-9ecbec37bd9f_1.5455ecd0d09089578a2c18e709910058.jpeg",
+    rating: {
+      rate: 3.6,
+      count: 145,
+    },
+  },
+  {
     id: 112,
     title: "Long Sleeve Jumper",
     price: 12.99,
     description:
       "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
     category: "women's clothing",
-    image: "https://www.fruitoftheloom.eu/wp-content/uploads/61-404-hd_dark-heather-grey_front.jpg",
+    image:
+      "https://www.fruitoftheloom.eu/wp-content/uploads/61-404-hd_dark-heather-grey_front.jpg",
     rating: {
       rate: 2,
       count: 145,
@@ -228,31 +238,35 @@ export const womenClothes = [
 ];
 
 export const kidsClothes = [
-    { 
-        id: 11,
-        title: "premium cotton baby's wear",
-        price: 55,
-        description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-        category: "jewelery",
-        image: "https://4.imimg.com/data4/IA/WC/MY-819866/ladies-printed-t-shirt-500x500.jpg",
-        rating: {
-        rate: 4.6,
-        count: 400
-        }
+  {
+    id: 11,
+    title: "premium cotton baby's wear",
+    price: 55,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    image:
+      "https://4.imimg.com/data4/IA/WC/MY-819866/ladies-printed-t-shirt-500x500.jpg",
+    rating: {
+      rate: 4.6,
+      count: 400,
     },
-    {
-        id: 12,
-        title: "pyjamas for your little kido",
-        price: 168,
-        description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-        category: "jewelery",
-        image: "https://5.imimg.com/data5/SELLER/Default/2021/11/FQ/NT/VO/2190204/s-bp-7-500x500.jpg",
-        rating: {
-        rate: 3.9,
-        count: 70
-        }
+  },
+  {
+    id: 12,
+    title: "pyjamas for your little kido",
+    price: 168,
+    description:
+      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    category: "jewelery",
+    image:
+      "https://5.imimg.com/data5/SELLER/Default/2021/11/FQ/NT/VO/2190204/s-bp-7-500x500.jpg",
+    rating: {
+      rate: 3.9,
+      count: 70,
     },
-  
+  },
+
   {
     id: 13,
     title: "for your Princess",
@@ -325,7 +339,7 @@ export const kidsClothes = [
     },
   },
   {
-    id: 13,
+    id: 200,
     title: "for your Princess",
     price: 9.99,
     description:
